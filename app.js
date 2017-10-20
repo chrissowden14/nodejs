5a8bb6e5cea3afbb49f9666d539846d738a84ad0
chrissowden14 <chrissowden@icloud.com>
2017-10-19 21:46:30 -0400

    added the self timer function

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ let bodyParser = require('body-parser');
 let path = require('path');
 let fs = require('fs');
 let jade = require('jade');
-
+let app = express();
 
 
 
 //read the JSON file
 let data = fs.readFileSync('server.json');
 let device = JSON.parse(data);
-console.log(device);
-let app = express();
 
+//Console logs
+console.log(device);
 console.log('Hello Mr. Ben Barbour  this Program is written by Chris Sowden, Chris Snyder, Cameron');
 console.log("Server running at http://localhost:3000/");
 
@@ -23,27 +23,48 @@ app.use(bodyParser.json());
 
 
 // use jade as the view engine
-//app.set('views', __dirname + 'views');
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(express.static(path.resolve(__dirname, "../public")));
 
 
 //using body parse
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+//Sever is started
+module.exports = app;
+app.listen(3000, 'localhost');
 
 
+//Routes to function calls
+app.get('/', setup);
+app.get('/temp', sendData);
+app.post('/temp', postToJson);
+app.get('/temp/latest',latest);
+app.get('/temp/highest', high);
+app.get('/temp/lowest', low);
+app.get('/temp/average', avg);
+app.get('/temp/:device_id', deviceID);
+app.get('/temp/:device_id/latest', id_latest);
+app.get('/temp/:device_id/highest', id_highest);
+app.get('/temp/:device_id/lowest', id_lowest);
+app.get('/temp/:device_id/average', id_average);
+app.get('*', error_message);
 
-//URL set to temp
-app.get('/', function (request, response) {
+
+setTimeout(Timer,2000);
+
+
+//function URL set to temp
+function setup (request, response) {
     response.redirect('/temp');
-});
+}
 
 
-//JSON is assigned an URL
-app.get('/temp', function (req, res) {
+// function to send data to local server
+function sendData (req, res) {
     id_array = [];
     device_temperature = [];
     for (let i = 0; i < device.length; i++) {
@@ -61,11 +82,35 @@ app.get('/temp', function (req, res) {
     fs.writeFile('server.json', str_add_data);
 
     res.send(device);
-});
 
+}
+
+// Auto timer function
+
+function Timer(){
+    id_array = [];
+    device_temperature = [];
+    for (let i = 0; i < device.length; i++) {
+        device_temperature.push(device[i].temperature);
+        id_array.push(device[i].device_id);
+    }
+    device_data = {
+        "device_id": (Math.floor(Math.random() * 20) + 1).toString(),
+        "timestamp": Math.floor(Date.now() / 1000),
+        "temperature": Math.floor(Math.random()*100)+32
+    };
+    device.push(device_data);
+
+    let str_add_data = JSON.stringify(device,null,2);
+    fs.writeFile('server.json', str_add_data);
+
+    let new_data = JSON.stringify(device_data,null,2);
+    console.log("Success: "+new_data);
+    setTimeout(Timer,4000);
+}
 
 //This is where it will post to TEMP
-app.post('/temp', function (req, res) {
+ function postToJson (req, res) {
     if (!req.body.device_id || !req.body.temperature) {
         res.status(400).send("This is the original settings");
         return;
@@ -77,12 +122,14 @@ app.post('/temp', function (req, res) {
         "temperature": req.body.temperature
     };
 
-
     res.redirect('/temp');
-});
+
+}
+
+
 
 // Retrieves the most recent from device
-app.get('/temp/latest', function (request, response) {
+ function latest (request, response) {
     highest = 0;
     for (let i = 0; i < device.length; i++) {
         if (device[i].timestamp > highest) {
@@ -91,10 +138,10 @@ app.get('/temp/latest', function (request, response) {
         }
     }
     response.send(value);
-});
+}
 
 // Retrieves the most recent from device
-app.get('/temp/highest', function (request, response) {
+ function high(request, response) {
     highest = 0;
     for (let i = 0; i < device.length; i++) {
         if (device[i].temperature > highest) {
@@ -105,10 +152,10 @@ app.get('/temp/highest', function (request, response) {
 
     response.send(sensor);
 
-});
+}
 
 //This will display the lowest temp
-app.get('/temp/lowest', function (request, response) {
+function low (request, response) {
     highest = 1000;
     for (let i = 0; i < device.length; i++) {
         if (device[i].temperature < highest) {
@@ -119,20 +166,20 @@ app.get('/temp/lowest', function (request, response) {
 
     response.send(sensor);
 
-});
+}
 
 //This will display average temperature
-app.get('/temp/average', function (request, response) {
+function avg(request, response) {
     total = 0;
     for (let i = 0; i < device.length; i++) {
         total += device[i].temperature;
     }
     average_temperature = total / device.length;
     response.send({average_temperature});
+}
 
-});
 //will display temperature of any given {device_id}
-app.get('/temp/:device_id', function (request, response, next) {
+ function deviceID (request, response, next) {
     sensor = [];
     for (let i = 0; i < device.length; i++) {
         if (device[i].device_id === request.params.device_id) {
@@ -149,11 +196,11 @@ app.get('/temp/:device_id', function (request, response, next) {
     }
 
     response.send(sensor);
-});
+}
 
 
 //latest temperature from {device_id}
-app.get('/temp/:device_id/latest', function (request, response, next) {
+function id_latest (request, response, next) {
     sensor = [];
     for (let i = device.length - 1; i > 0; i--) {
 
@@ -170,9 +217,11 @@ app.get('/temp/:device_id/latest', function (request, response, next) {
     }
 
     response.send(sensor);
-});
+}
+
+
 //Highest temperature from {device_id}
-app.get('/temp/:device_id/highest', function (request, response, next) {
+function id_highest (request, response, next) {
     sensor = [];
     for (let i = 0; i < device.length; i++) {
         if (device[i].device_id === request.params.device_id) {
@@ -196,9 +245,9 @@ app.get('/temp/:device_id/highest', function (request, response, next) {
     }
 
     response.end("Error");
-});
+}
 //lowest temperature from {device_id}
-app.get('/temp/:device_id/lowest', function (request, response, next) {
+ function id_lowest (request, response, next) {
     sensor = [];
     for (let i = 0; i < device.length; i++) {
         if (device[i].device_id === request.params.device_id) {
@@ -222,9 +271,9 @@ app.get('/temp/:device_id/lowest', function (request, response, next) {
     }
 
     response.end("Error");
-});
+}
 //Average temperature from {device_id}
-app.get('/temp/:device_id/average', function (request, response, next) {
+function id_average (request, response, next) {
     sensor = [];
     total= 0;
     count = 0;
@@ -251,17 +300,15 @@ app.get('/temp/:device_id/average', function (request, response, next) {
 
     avgerage_temperature = total / count;
     response.send({avgerage_temperature});
-});
+}
 
 
 
 //404 route
-app.get('*', function (request, response) {
+function error_message (request, response) {
     response.status(404).send('ERROR 404 PAGE NOT FOUND' );
-});
-//Sever is started
-module.exports = app;
-app.listen(3000, 'localhost');
+}
+
 
 
 
